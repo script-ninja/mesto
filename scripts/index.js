fa145250f1d27750a770a02d9db7f2a608ed5dbb
script-ninja <61618647+script-ninja@users.noreply.github.com>
2020-08-10 23:08:51 +0500

    fix: Рефакторинг кода

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,5 @@
 const root = document.querySelector('.root');
 
-const popup = document.querySelector('.popup');
-
-const formProfile = popup.querySelector('#form-profile');
-const formProfileUserName = formProfile.querySelector('input[name="user-name"]');
-const formProfileUserHobby = formProfile.querySelector('input[name="user-hobby"]');
-const formProfileButtonSubmit = formProfile.querySelector('.form__button-submit');
-
-const formPlace = popup.querySelector('#form-place');
-const formPlaceName = formPlace.querySelector('input[name="place-name"]');
-const formPlaceLink = formPlace.querySelector('input[name="place-link"]');
-const formPlaceButtonSubmit = formPlace.querySelector('.form__button-submit');
-
 const profile = root.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
 const profileCareer = profile.querySelector('.profile__career');
@@ -20,92 +8,109 @@ const profileButtonAdd = profile.querySelector('.profile__add-button');
 
 const gallery = root.querySelector('.gallery');
 const cardTemplate = gallery.querySelector('#photo-card').content;
-const photoTemplate = popup.querySelector('#photo').content;
+
+const popupProfile = document.querySelector('.popup[data-type="profile"]');
+const formProfile = popupProfile.querySelector('#form-profile');
+const formProfileUserName = formProfile.querySelector('input[name="user-name"]');
+const formProfileUserHobby = formProfile.querySelector('input[name="user-hobby"]');
+
+const popupPlace = document.querySelector('.popup[data-type="place"]');
+const formPlace = popupPlace.querySelector('#form-place');
+const formPlaceName = formPlace.querySelector('input[name="place-name"]');
+const formPlaceLink = formPlace.querySelector('input[name="place-link"]');
+
+const popupPhoto = document.querySelector('.popup[data-type="photo"]');
+const photoImage = popupPhoto.querySelector('.photo__image');
+const photoTitle = popupPhoto.querySelector('.photo__title');
 
 
-const addCard = function (card, setCardPlace = gallery.prepend) {
+const togglePopup = function (popup) {
+  popup.classList.toggle('popup_visible');
+}
+
+
+const openProfileForm = function (event) {
+  formProfileUserName.value = profileName.textContent;
+  formProfileUserHobby.value = profileCareer.textContent;
+  togglePopup(popupProfile);
+}
+
+const saveProfile = function (event) {
+  event.preventDefault();
+  profileName.textContent = formProfileUserName.value;
+  profileCareer.textContent = formProfileUserHobby.value;
+  togglePopup(popupProfile);
+}
+
+
+const openPlaceForm = function (event) {
+  togglePopup(popupPlace);
+}
+
+const savePlace = function (event) {
+  event.preventDefault();
+  addPhotoCard({
+    name: formPlaceName.value,
+    link: formPlaceLink.value,
+    like: false
+  });
+  formPlaceName.value = '';
+  formPlaceLink.value = '';
+  togglePopup(popupPlace);
+}
+
+
+const openPhoto = function (event) {
+  photoImage.src = event.target.src;
+  photoImage.alt = event.target.alt;
+  photoTitle.textContent = event.target.alt;
+  togglePopup(popupPhoto);
+}
+
+
+const createPhotoCard = function (card) {
   const newCard = cardTemplate.cloneNode(true);
   newCard.querySelector('.photo-card__title').textContent = card.name;
   newCard.querySelector('.photo-card__title').title = card.name;
   newCard.querySelector('.photo-card__image').alt = card.name;
   newCard.querySelector('.photo-card__image').src = card.link;
-  newCard.querySelector('.photo-card__image').addEventListener('click', togglePopup);
-  if (card.like) {
-    newCard.querySelector('.photo-card__like-button').classList.add('photo-card__like-button_liked');
-  }
+  newCard.querySelector('.photo-card__image').addEventListener('click', openPhoto);
+  // Здравствуйте, Иван! В этом месте я не понял насчёт лайков.
+  // Сейчас базу данных имитирует массив. Я в этот массив добавил новое поле хранящее лайк.
+  // Таким образом, создается некое подобие хранения данных и их загрузки с сервера.
+  // Или этот массив для чего-то другого предназначен?
+  //
+  // Ну, а в целом, спасибо за замечания! Даже мне самому стало легче читать собственный код. xD
+  //
+  // if (card.like) {
+  //   newCard.querySelector('.photo-card__like-button').classList.add('photo-card__like-button_liked');
+  // }
   newCard.querySelector('.photo-card__like-button').addEventListener('click', (event) => {
     event.target.classList.toggle('photo-card__like-button_liked');
   });
   newCard.querySelector('.photo-card__del-button').addEventListener('click', (event) => {
     event.target.closest('.photo-card').remove();
   });
-  setCardPlace.call(gallery, newCard);
+  return newCard;
+}
+
+const addPhotoCard = function (card, prepend = true) {
+  (prepend) ? gallery.prepend(createPhotoCard(card)) : gallery.append(createPhotoCard(card));
 }
 
 
-const togglePopup = function (event) {
-  if (!popup.classList.contains('popup_visible')) {
-    switch (event.target) {
-      case profileButtonEdit:
-        formProfileUserName.value = profileName.textContent;
-        formProfileUserHobby.value = profileCareer.textContent;
-        formProfile.classList.add('form_active');
-        break;
-      case profileButtonAdd:
-        formPlace.classList.add('form_active');
-        break;
-      default:
-        const photo = photoTemplate.cloneNode(true);
-        photo.querySelector('.photo__title').textContent = event.target.alt;
-        photo.querySelector('.photo__image').src = event.target.src;
-        photo.querySelector('.photo__image').alt = event.target.alt;
-        popup.append(photo);
-        popup.classList.add('popup_type_photo');
-        break;
-    }
-    popup.classList.add('popup_visible');
-  }
-  else {
-    if (event.target.classList.contains('popup__button-close') || event.target === event.currentTarget) {
-      setTimeout(() => {
-        switch(popup.lastElementChild) {
-          case popup.querySelector('.photo'):
-            popup.querySelector('.photo').remove();
-            popup.classList.remove('popup_type_photo');
-            break;
-          default:
-            popup.querySelector('.form_active').classList.remove('form_active');
-            break;
-        }
-      }, 300); // задержка в соответствии с длительностью анимации
-      popup.classList.remove('popup_visible');
-    }
-  }
-}
-
-
-const submitForm = function (event) {
-  event.preventDefault();
-  switch (event.target) {
-    case formProfile:
-      profileName.textContent = formProfileUserName.value;
-      profileCareer.textContent = formProfileUserHobby.value;
-      break;
-    case formPlace:
-      addCard({
-        name: formPlaceName.value,
-        link: formPlaceLink.value,
-        like: false
-      });
-      break;
-  }
-  togglePopup(event);
-}
-
-
-initialCards.forEach(card => { addCard(card, gallery.append); });
-profileButtonEdit.addEventListener('click', togglePopup);
-profileButtonAdd.addEventListener('click', togglePopup);
-formProfile.addEventListener('submit', submitForm);
-formPlace.addEventListener('submit', submitForm);
-popup.addEventListener('click', togglePopup);
+initialCards.forEach(card => { addPhotoCard(card, false); });
+profileButtonEdit.addEventListener('click', openProfileForm);
+profileButtonAdd.addEventListener('click', openPlaceForm);
+formProfile.addEventListener('submit', saveProfile);
+formPlace.addEventListener('submit', savePlace);
+document.querySelectorAll('.popup').forEach((popup) => {
+  popup.addEventListener('click', (event) => {
+    if (event.target === event.currentTarget) togglePopup(popup);
+  });
+});
+document.querySelectorAll('.popup__button-close').forEach((closeButton) => {
+  closeButton.addEventListener('click', (event) => {
+    togglePopup(event.target.closest('.popup'));
+  });
+});
