796dc7f7c00724af04b9f909b5c411aa37e9d87c
script-ninja <61618647+script-ninja@users.noreply.github.com>
2020-10-11 14:46:07 +0500

    chore: новая файловая структура

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,33 +8,21 @@ import UserInfo from './UserInfo.js';
 
 
 // Объявления переменных -----------------------------------------------------
-const root = document.querySelector('.root');
-
-const profile = root.querySelector('.profile');
-const profileButtonEdit = profile.querySelector('.profile__edit-button');
-const profileButtonAdd = profile.querySelector('.profile__add-button');
-
-const formProfile = document.forms.profile;
-const formProfileName = formProfile.elements['user-name'];
-const formProfileHobby = formProfile.elements['user-hobby'];
-
-const formPlace = document.forms.place;
-
 const validatorSettings = {
   inputSelector: '.form__text',
   invalidInputClass: 'form__text_invalid',
   submitSelector: '.form__button-submit',
   submitDisabledClass: 'form__button-submit_disabled'
 };
-formPlace.validator = new FormValidator(validatorSettings, formPlace);
-formProfile.validator = new FormValidator(validatorSettings, formProfile);
+const formPlaceValidator = new FormValidator(validatorSettings, document.forms.place);
+const formProfileValidator = new FormValidator(validatorSettings, document.forms.profile);
 
 const userInfo = new UserInfo({
   userNameSelector: '.profile__name',
   userInfoSelector: '.profile__career'
 });
 
-// ___ Popups ___
+// Popups -----
 const popupWithFormProfile = new PopupWithForm(
   '.popup[data-type="profile"]',
   function(event) {
@@ -57,7 +45,7 @@ const popupWithFormPlace = new PopupWithForm(
 );
 
 const popupWithImage = new PopupWithImage('.popup[data-type="photo"]', '.photo__image', '.photo__title');
-// ______________
+// ------------
 
 const sectionGallery = new Section(
   {
@@ -85,26 +73,20 @@ const sectionGallery = new Section(
 );
 
 
-// Объявления функций --------------------------------------------------------
-const openProfileForm = function (event) {
-  formProfile.validator.clearStatus();
-  const { name, info } = userInfo.getUserInfo();
-  formProfileName.value = name;
-  formProfileHobby.value = info;
-  popupWithFormProfile.open();
-}
-
-const openPlaceForm = function (event) {
-  formPlace.validator.clearStatus();
-  popupWithFormPlace.open();
-}
-
-
 // Точка входа ---------------------------------------------------------------
 window.onload = function() {
+  document.querySelector('.profile__edit-button').addEventListener('click', () => {
+    formProfileValidator.clearStatus();
+    const { name, info } = userInfo.getUserInfo();
+    document.forms.profile.elements['user-name'].value = name;
+    document.forms.profile.elements['user-hobby'].value = info;
+    popupWithFormProfile.open();
+  });
+  document.querySelector('.profile__add-button').addEventListener('click', () => {
+    formPlaceValidator.clearStatus();
+    popupWithFormPlace.open();
+  });
   sectionGallery.renderItems();
-  profileButtonEdit.addEventListener('click', openProfileForm);
-  profileButtonAdd.addEventListener('click', openPlaceForm);
-  formProfile.validator.enableValidation();
-  formPlace.validator.enableValidation();
+  formProfileValidator.enableValidation();
+  formPlaceValidator.enableValidation();
 }
