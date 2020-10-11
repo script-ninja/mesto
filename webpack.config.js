aa47537e80b22c2981c105ecd8c2a1bd7e530501
script-ninja <61618647+script-ninja@users.noreply.github.com>
2020-10-11 18:54:10 +0500

    chore: добавлен webpack + babel

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: { // точка входа
+    main: './src/pages/index.js'
+  },
+  output: { // точка выхода
+    path: path.resolve(__dirname, 'dist'), // __dirname - глобальная переменная в Node.js, содержит абсолютный путь до текущей рабочей директории
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      // rules — это массив правил
+      {
+        // добавим в него объект правил для бабеля
+        test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+        loader: 'babel-loader', // при обработке этих файлов нужно использовать babel-loader
+        exclude: '/node_modules/' // исключает папку node_modules, файлы в ней обрабатывать не нужно
+      }
+    ]
+  }
+}
