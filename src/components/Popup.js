36ab06d21eff0764023107e1e683ad16dc099abc
script-ninja <61618647+script-ninja@users.noreply.github.com>
2020-10-20 22:09:32 +0500

    fix: неправильная связка обработчика Esc с открытым попапом

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,6 +1,7 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   _handleEscClose(event) {
@@ -19,7 +20,7 @@ export default class Popup {
 
   open() {
     this._popup.classList.add('popup_visible');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
